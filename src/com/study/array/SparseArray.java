acf42f30699571f89df04bdace9467713b8dc218
zhangke <15528127727@163.com>
2020-05-06 16:11:25 +0800

    更新

---
+++ src/com/study/array/SparseArray.java
@@ -0,0 +1,70 @@
+package com.study.array;
+
+public class SparseArray {
+    //稀疏数组
+    public static  void  main(String[] args){
+        //创建一个原始的二维数组
+        //0表示没有棋子，1表示黑子，2表示蓝子
+        int chessArr1[][]=new int[11][11];
+        chessArr1[1][2]=1;
+        chessArr1[2][3]=2;
+        //输出原始数组
+        System.out.println("原始的二维数组");
+        for(int[] row : chessArr1){
+            for(int data:row){
+                System.out.printf("%d \t",data);
+            }
+            System.out.println();
+        }
+
+        //将二维数组转换为稀疏数组
+        //先遍历得到非零个数
+        int sum=0;
+        for (int i=0;i<11;i++){
+            for (int j=0;j<11;j++){
+                if (chessArr1[i][j]!=0){
+                    sum++;
+                }
+            }
+        }
+        //创建对应的稀疏数组
+        int sparseArr[][]=new int[sum+1][3];
+        //给稀疏数组赋值
+        sparseArr[0][0]=11;
+        sparseArr[0][1]=11;
+        sparseArr[0][2]=sum;
+        //遍历二维数组
+        int count=0;//用于记录是第几个非零的数
+        for (int i=0;i<11;i++){
+            for (int j=0;j<11;j++){
+                if (chessArr1[i][j]!=0){
+                    count++;
+                    sparseArr[count][0]=i;
+                    sparseArr[count][1]=j;
+                    sparseArr[count][2]=chessArr1[i][j];
+                }
+            }
+        }
+        //输出稀疏数组
+        System.out.println();
+        System.out.println("得到稀疏数组为：");
+        for (int i = 0; i <sparseArr.length ; i++) {
+            System.out.printf("%d\t%d\t%d\t\n",sparseArr[i][0],sparseArr[i][1],sparseArr[i][2]);
+        }
+        System.out.println();
+        //二维数组--》恢复成原始的二维数组
+        int chessArr2[][]= new int[sparseArr[0][0]][sparseArr[0][1]];
+        for (int i = 1; i <sparseArr.length ; i++) {
+            chessArr2[sparseArr[i][0]][sparseArr[i][1]]=sparseArr[i][2];
+        }
+        System.out.println();
+        System.out.println("恢复后的二维数组");
+        for(int[] row : chessArr2){
+            for(int data:row){
+                System.out.printf("%d \t",data);
+            }
+            System.out.println();
+        }
+
+    }
+}
